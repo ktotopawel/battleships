323eae53ebdf6ba413eb4d8cdc63a602e9931547
ktotopawel <pawel.juszczak02@gmail.com>
2025-04-07 15:54:13 +0200

    general ui layout and interactivity

--- src/index.js
+++ src/index.js
@@ -5,12 +5,7 @@ import { Player, CPUPlayer } from "./board.js";
 const playerOne = new Player("one");
 const playerTwo = new Player("two");
 
-playerOne.gameboard.placeShip({ y: 2, x: 5 }, { y: 5, x: 5 });
-playerTwo.gameboard.placeShip({ y: 3, x: 3 }, { y: 3, x: 3 });
-playerTwo.gameboard.recieveAttack({ y: 3, x: 3 });
-playerTwo.gameboard.recieveAttack({ y: 5, x: 5 });
-playerOne.gameboard.recieveAttack({ y: 2, x: 5 });
-playerOne.gameboard.recieveAttack({ y: 1, x: 1 });
+playerOne.gameboard.placeShip({ x: 3, y: 0 }, { x: 5, y: 0 });
 
 const boardHandler = new BoardHandler(playerOne, playerTwo);
 
