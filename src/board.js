894edad3e469491ed853c63ccd960fd71bd77119
ktotopawel <pawel.juszczak02@gmail.com>
2025-04-22 15:14:58 +0200

    random placement

--- src/board.js
+++ src/board.js
@@ -56,27 +56,32 @@ class Gameboard {
   }
 
   placeShip(start, end) {
-    if (!this.validateCoordinates(start, end))
-      throw new Error("invalid coordinates");
+    if (!this.validateCoordinates(start, end)) return false;
 
     const isHorizontal = start.y === end.y;
     const isVertical = start.x === end.x;
 
     if (!isHorizontal && !isVertical) throw new Error("invalid ship placement");
 
+    const coordinateArr = [];
+    const length = isHorizontal
+      ? Math.abs(end.x - start.x) + 1
+      : Math.abs(end.y - start.y) + 1;
+    const thisShip = new Ship(length);
+
     if (isHorizontal) {
-      const length = Math.abs(end.x - start.x + 1);
-      const thisShip = new Ship(length);
       for (let x = start.x; x <= end.x; x++) {
-        this.grid[start.y][x].contains = thisShip;
+        coordinateArr.push(this.grid[start.y][x]);
       }
     } else {
-      const length = Math.abs(end.y - start.y + 1);
-      const thisShip = new Ship(length);
       for (let y = start.y; y <= end.y; y++) {
-        this.grid[y][start.x].contains = thisShip;
+        coordinateArr.push(this.grid[y][start.x]);
       }
     }
+
+    if (!coordinateArr.every((cell) => !cell.contains)) return false;
+    coordinateArr.forEach((cell) => (cell.contains = thisShip));
+    return true;
   }
 
   validateCoordinates(...args) {
@@ -112,6 +117,34 @@ class Player {
     (this.name = name),
       (this.gameboard = new Gameboard(boardSize, boardSize)),
       (this.isCurrentPlayer = false);
+  }
+
+  randomizePlacement() {
+    const placeRandomShip = (length) => {
+      const randomCoordinates = {
+        x: Math.floor(Math.random() * 10),
+        y: Math.floor(Math.random() * 10),
+      };
+      const isHorizontal = Math.random > 0.5 ? true : false;
+
+      const endCooridantes = isHorizontal
+        ? { x: randomCoordinates.x + length - 1, y: randomCoordinates.y }
+        : { x: randomCoordinates.x, y: randomCoordinates.y + length - 1 };
+
+      if (this.gameboard.placeShip(randomCoordinates, endCooridantes)) {
+        return;
+      } else {
+        placeRandomShip(length);
+      }
+    };
+
+    placeRandomShip(1);
+    placeRandomShip(1);
+    placeRandomShip(2);
+    placeRandomShip(2);
+    placeRandomShip(3);
+    placeRandomShip(4);
+    placeRandomShip(5);
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,70 +1,12 @@
 import "./style.css";
-// import BoardHandler, { ButtonsHandler } from "./domhandler.js";
-// import { Player } from "./board.js";
-
-// const playerOne = new Player("one");
-// const playerTwo = new Player("two");
-
-// playerOne.isCurrentPlayer = true;
-
-// const boardHandler = new BoardHandler(playerOne, playerTwo);
-
-// playerOne.gameboard.placeShip({ y: 0, x: 0 }, { y: 0, x: 1 });
-// playerOne.gameboard.placeShip({ y: 0, x: 3 }, { y: 2, x: 3 });
-// playerOne.gameboard.placeShip({ y: 0, x: 6 }, { y: 0, x: 9 });
-// playerOne.gameboard.placeShip({ y: 2, x: 0 }, { y: 5, x: 0 });
-// playerOne.gameboard.placeShip({ y: 2, x: 5 }, { y: 2, x: 7 });
-// playerOne.gameboard.placeShip({ y: 2, x: 9 }, { y: 3, x: 9 });
-// playerOne.gameboard.placeShip({ y: 5, x: 5 }, { y: 6, x: 5 });
-// playerOne.gameboard.placeShip({ y: 5, x: 9 }, { y: 7, x: 9 });
-// playerOne.gameboard.placeShip({ y: 7, x: 0 }, { y: 7, x: 1 });
-// playerOne.gameboard.placeShip({ y: 9, x: 4 }, { y: 9, x: 9 });
-
-// playerTwo.gameboard.placeShip({ y: 0, x: 0 }, { y: 0, x: 1 });
-// playerTwo.gameboard.placeShip({ y: 0, x: 3 }, { y: 2, x: 3 });
-// playerTwo.gameboard.placeShip({ y: 0, x: 6 }, { y: 0, x: 9 });
-// playerTwo.gameboard.placeShip({ y: 2, x: 0 }, { y: 5, x: 0 });
-// playerTwo.gameboard.placeShip({ y: 2, x: 5 }, { y: 2, x: 7 });
-// playerTwo.gameboard.placeShip({ y: 2, x: 9 }, { y: 3, x: 9 });
-// playerTwo.gameboard.placeShip({ y: 5, x: 5 }, { y: 6, x: 5 });
-// playerTwo.gameboard.placeShip({ y: 5, x: 9 }, { y: 7, x: 9 });
-// playerTwo.gameboard.placeShip({ y: 7, x: 0 }, { y: 7, x: 1 });
-// playerTwo.gameboard.placeShip({ y: 9, x: 4 }, { y: 9, x: 9 });
-
-// boardHandler.generateGrids(playerOne.gameboard.grid, playerTwo.gameboard.grid);
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const btns = new ButtonsHandler();
-//   btns.bindStarts();
-//   btns.bindSwitchPlayer();
-// });
 
 import GameController from "./domhandler.js";
 
 const newGame = new GameController();
 newGame.player1.isCurrentPlayer = true;
 
-newGame.player1.gameboard.placeShip({ y: 0, x: 0 }, { y: 0, x: 1 });
-newGame.player1.gameboard.placeShip({ y: 0, x: 3 }, { y: 2, x: 3 });
-newGame.player1.gameboard.placeShip({ y: 0, x: 6 }, { y: 0, x: 9 });
-newGame.player1.gameboard.placeShip({ y: 2, x: 0 }, { y: 5, x: 0 });
-newGame.player1.gameboard.placeShip({ y: 2, x: 5 }, { y: 2, x: 7 });
-newGame.player1.gameboard.placeShip({ y: 2, x: 9 }, { y: 3, x: 9 });
-newGame.player1.gameboard.placeShip({ y: 5, x: 5 }, { y: 6, x: 5 });
-newGame.player1.gameboard.placeShip({ y: 5, x: 9 }, { y: 7, x: 9 });
-newGame.player1.gameboard.placeShip({ y: 7, x: 0 }, { y: 7, x: 1 });
-newGame.player1.gameboard.placeShip({ y: 9, x: 4 }, { y: 9, x: 9 });
-
-newGame.player2.gameboard.placeShip({ y: 0, x: 0 }, { y: 0, x: 1 });
-newGame.player2.gameboard.placeShip({ y: 0, x: 3 }, { y: 2, x: 3 });
-newGame.player2.gameboard.placeShip({ y: 0, x: 6 }, { y: 0, x: 9 });
-newGame.player2.gameboard.placeShip({ y: 2, x: 0 }, { y: 5, x: 0 });
-newGame.player2.gameboard.placeShip({ y: 2, x: 5 }, { y: 2, x: 7 });
-newGame.player2.gameboard.placeShip({ y: 2, x: 9 }, { y: 3, x: 9 });
-newGame.player2.gameboard.placeShip({ y: 5, x: 5 }, { y: 6, x: 5 });
-newGame.player2.gameboard.placeShip({ y: 5, x: 9 }, { y: 7, x: 9 });
-newGame.player2.gameboard.placeShip({ y: 7, x: 0 }, { y: 7, x: 1 });
-newGame.player2.gameboard.placeShip({ y: 9, x: 4 }, { y: 9, x: 9 });
+newGame.player1.randomizePlacement();
+newGame.player2.randomizePlacement();
 
 newGame.boardHandler.generateGrids(
   newGame.player1.gameboard.grid,
